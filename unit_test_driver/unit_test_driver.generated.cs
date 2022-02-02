d2604c1bf67a5b6c81be2e32b637b414c39a049c
Fran Martinez Fadrique <ffadrique@gmail.com>
2022-02-02 20:23:31 +0100

    Initial commit

---
+++ unit_test_driver/unit_test_driver.generated.cs
@@ -0,0 +1,449 @@
+
+
+
+
+
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestDriver
+{
+
+
+		[TestClass]
+		public class xfunit : Unit
+		{
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_array_character()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_array_character");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_array_character"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_array_class()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_array_class");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_array_class"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_array_complex()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_array_complex");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_array_complex"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_array_integer()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_array_integer");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_array_integer"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_array_integer_between()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_array_integer_between");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_array_integer_between"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_array_integer_greater()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_array_integer_greater");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_array_integer_greater"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_array_integer_less()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_array_integer_less");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_array_integer_less"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_array_logical()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_array_logical");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_array_logical"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_array_real()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_array_real");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_array_real"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_array_real_between()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_array_real_between");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_array_real_between"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_array_real_greater()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_array_real_greater");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_array_real_greater"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_array_real_less()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_array_real_less");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_array_real_less"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_array_string()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_array_string");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_array_string"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_character()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_character");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_character"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_class()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_class");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_class"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_complex()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_complex");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_complex"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_files()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_files");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_files"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_integer()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_integer");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_integer"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_integer_between()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_integer_between");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_integer_between"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_integer_greater()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_integer_greater");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_integer_greater"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_integer_less()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_integer_less");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_integer_less"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_logical()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_logical");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_logical"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_matrix_class()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_matrix_class");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_matrix_class"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_matrix_integer()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_matrix_integer");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_matrix_integer"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_matrix_real()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_matrix_real");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_matrix_real"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_real()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_real");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_real"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_real_between()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_real_between");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_real_between"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_real_greater()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_real_greater");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_real_greater"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_real_less()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_real_less");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_real_less"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_string()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_string");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_string"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_assertion_write()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_assertion_write");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_assertion_write"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_suite()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_suite");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_suite"), 0);
+				}
+
+				[TestMethod]
+				public void unit_m_xfunit_unit()
+				{
+					// Initialise the test run
+					Initialise("unit_m_xfunit_unit");
+
+					// Execute the fortran test if required
+					if(OutOfDate) RunFortranTest();
+
+					// Assert the test completion
+					Assert.AreEqual(ProcessJxmlFile("m_xfunit_unit"), 0);
+				}
+
+		}
+
+}
